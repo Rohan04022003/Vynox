5146b0522d88a4c9d0537ddf7a0773bd0b0a045a
Rohan Kumar Mahto <rohankumar993985@gmail.com>
2025-10-04 17:19:34 +0530

    refactor getChannelVideos to implement aggregation for enhanced video retrieval with owner details

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
+import { Video } from "../models/video.model.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
@@ -98,6 +99,34 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+
+  try {
+    const videos = await Video.aggregate([
+      {
+        $lookup: {
+          from: "users",
+          localField: "owner",
+          foreignField: "_id",
+          as: "owner",
+          pipeline: [
+            {
+              $project: {
+                username: 1,
+                fullName: 1,
+                avatar: 1,
+              },
+            },
+          ],
+        },
+      },
+    ]);
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, videos, "All videos fetched successfully."));
+  } catch (error) {
+    throw new ApiError(500, "Something was wrong while fetching all videos.");
+  }
 });
 
 export { getChannelStats, getChannelVideos };
