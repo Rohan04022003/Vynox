8629dfeba60145daaf169565e16a91dead0e1703
Rohan Kumar Mahto <rohankumar993985@gmail.com>
2025-10-06 13:49:20 +0530

    refactor addVideoToPlaylist to enhance error handling and transaction management; update playlist routes for consistency

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,8 +1,9 @@
-import mongoose, { isValidObjectId } from "mongoose";
+import mongoose, { isValidObjectId, mongo } from "mongoose";
 import { Playlist } from "../models/playlist.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
@@ -38,7 +39,74 @@ const getPlaylistById = asyncHandler(async (req, res) => {
 });
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-  const { playlistId, videoId } = req.params;
+  const { playlistId } = req.params;
+  const { videos } = req.body;
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  if (!mongoose.Types.ObjectId.isValid(playlistId)) {
+    throw new ApiError(400, "playlistId is not valid.");
+  }
+
+  if (!videos || (Array.isArray(videos) && videos.length === 0)) {
+    throw new ApiError(400, "No videos provided to add.");
+  }
+
+  const videoIds = Array.isArray(videos) ? videos : [videos];
+
+  try {
+    for (let id of videoIds) {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new ApiError(400, `Invalid videoId: ${id}`);
+      }
+    }
+
+    const existingVideos = await Video.find({
+      _id: { $in: videoIds },
+      owner: req.user?._id,
+    }).session(session);
+    if (existingVideos.length !== videoIds.length) {
+      throw new ApiError(400, "Some videos was not found.");
+    }
+
+    const playlist = await Playlist.findOneAndUpdate(
+      { _id: playlistId, owner: req.user?._id },
+      {
+        $addToSet: { videos: { $each: videoIds } },
+      },
+      {
+        new: true,
+        session,
+      }
+    );
+
+    await session.commitTransaction();
+    session.endSession();
+
+    if (!playlist) {
+      throw new ApiError(
+        404,
+        "Playlist not found or not authorized to update."
+      );
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, playlist, "Videos added to playlist successfully.")
+      );
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
+    throw new ApiError(
+      500,
+      "Something was wrong while add videos into playlist."
+    );
+  } finally {
+    session.endSession();
+  }
 });
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
@@ -78,6 +146,51 @@ const updatePlaylist = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
   const { name, description } = req.body;
   //TODO: update playlist
+
+  let updated = {};
+
+  if (!mongoose.Types.ObjectId.isValid(playlistId)) {
+    throw new ApiError(400, "PlaylistId is not valid.");
+  }
+
+  if (name && name.trim()) {
+    updated.name = name.trim();
+  }
+
+  if (description && description.trim()) {
+    updated.description = description.trim();
+  }
+
+  if (Object.keys(updated).length === 0) {
+    throw new ApiError(400, "No valid fields to update.");
+  }
+
+  let playlist = {};
+
+  try {
+    if (updated.name || updated.description) {
+      playlist = await Playlist.findOneAndUpdate(
+        { _id: playlistId, owner: req.user?._id },
+        { $set: updated },
+        { new: true }
+      );
+    }
+
+    if (!playlist) {
+      throw new ApiError(
+        404,
+        "Playlist not found or not authorized to update."
+      );
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, playlist, "Playlist was updated successfully.")
+      );
+  } catch (error) {
+    throw new ApiError(500, "Something was wrong while updating playlist.");
+  }
 });
 
 export {
