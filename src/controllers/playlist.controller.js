26437869904094ee8f5fafa0f4a59668b4133d9d
Rohan Kumar Mahto <rohankumar993985@gmail.com>
2025-10-06 13:49:20 +0530

    refactor updatePlaylist to enhance error handling and response structure

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -78,6 +78,51 @@ const updatePlaylist = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
   const { name, description } = req.body;
   //TODO: update playlist
+
+  let updated = {};
+
+  if (!mongoose.Types.ObjectId.isValid(playlistId)) {
+    throw new ApiError(400, "PlaylistId is not valid.");
+  }
+
+  if (name && name.trim()) {
+    updated.name = name.trim();
+  }
+
+  if (description && description.trim()) {
+    updated.description = description.trim();
+  }
+
+  if (Object.keys(updated).length === 0) {
+    throw new ApiError(400, "No valid fields to update.");
+  }
+
+  let playlist = {};
+
+  try {
+    if (updated.name || updated.description) {
+      playlist = await Playlist.findOneAndUpdate(
+        { _id: playlistId, owner: req.user?._id },
+        { $set: updated },
+        { new: true }
+      );
+    }
+
+    if (!playlist) {
+      throw new ApiError(
+        404,
+        "Playlist not found or not authorized to update."
+      );
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, playlist, "Playlist was updated successfully.")
+      );
+  } catch (error) {
+    throw new ApiError(500, "Something was wrong while updating playlist.");
+  }
 });
 
 export {
