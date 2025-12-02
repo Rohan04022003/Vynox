fe03cee74c0ba9a7194eaf2ce55f532da6173f57
Rohan Kumar Mahto <rohankumar993985@gmail.com>
2025-12-02 12:28:47 +0530

    add comments in videosContext file

--- vynox-frontend/src/context/VideosContext.tsx
+++ vynox-frontend/src/context/VideosContext.tsx
@@ -12,40 +12,40 @@ import type { Video, VideosContextType } from "../types";
 const VideosContext = createContext<VideosContextType | undefined>(undefined);
 
 export const VideosProvider = ({ children }: { children: ReactNode }) => {
-  const [videos, setVideos] = useState<Video[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState<number>(1);
-  const [hasMore, setHasMore] = useState(true);
+  const [videos, setVideos] = useState<Video[]>([]); // yeh useState vidoes ko hold karega.
+  const [loading, setLoading] = useState(false); // yeh loading screen ke liye bana hai.
+  const [page, setPage] = useState<number>(1); // yeh by default page 1 karega.
+  const [hasMore, setHasMore] = useState(true); // iska use hamne aur content next page pe hai ya nahi uske liye use kiya hai.
 
-  const fetchVideos = async (
+  const fetchVideos = async ( // videos ko fetch karega yeh method.
     str = "",
     sortType = "desc",
     limit = 10,
     newPage?: number
   ) => {
-    const pageToFetch = newPage ?? page;
+    const pageToFetch = newPage ?? page; // yeh hamare current page number ko set krega.
 
     try {
       setLoading(true);
 
       const res = await axios.get(
-        `${import.meta.env.VITE_BASE_URL}/videos`,
+        `${import.meta.env.VITE_BASE_URL}/videos`, // yeh videos ka acutal url hai.
         {
-          params: { query: str, sortType, limit, page: pageToFetch },
+          params: { query: str, sortType, limit, page: pageToFetch }, // query hai videos ko fetch krne ke liye.
           withCredentials: true,
         }
       );
 
-      const fetchedVideos: Video[] = res.data?.data?.videos ?? [];
+      const fetchedVideos: Video[] = res.data?.data?.videos ?? []; // yaha pe fetchedVideos me woh filterd aur fetched videos aayenge.
 
       if (newPage) {
-        setVideos(fetchedVideos);
+        setVideos(fetchedVideos); // videos ko setVideos me set kiya hai.
         setPage(2);
-        setHasMore(fetchedVideos.length === limit);
+        setHasMore(fetchedVideos.length === limit); // yeh check krega ki aur content hai ya nahi means next page.
       } else {
-        setVideos(prev => [...prev, ...fetchedVideos]);
+        setVideos(prev => [...prev, ...fetchedVideos]); // next page ke content ko aad kiya hai.
         setPage(pageToFetch + 1);
-        setHasMore(fetchedVideos.length === limit);
+        setHasMore(fetchedVideos.length === limit); // yeh check krega ki aur content hai ya nahi means next page.
       }
     } catch (error) {
       console.error("Error fetching videos:", error);
