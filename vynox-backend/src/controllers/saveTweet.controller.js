7e6a1b74abff85c6043e075a7a3f22a8d2845337
Rohan Kumar Mahto <rohankumar993985@gmail.com>
2026-01-20 13:12:43 +0530

    feat: implement getSavedTweet functionality with aggregation and user-specific data

--- vynox-backend/src/controllers/saveTweet.controller.js
+++ vynox-backend/src/controllers/saveTweet.controller.js
@@ -54,4 +54,121 @@ const unsaveTweet = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, {}, "Tweet was unsaved"));
 });
 
+const getSavedTweet = asyncHandler(async (req, res) => {
+  const { sortType = "desc", limit = 20, page = 1 } = req.query;
+
+  const limitNum = Number(limit);
+  const pageNum = Number(page);
+  const skip = (pageNum - 1) * limitNum;
+
+  const userId = new mongoose.Types.ObjectId(req.user._id);
+
+  const result = await SaveTweet.aggregate([
+    // current user ka hi saved tweet aaye uske liye.
+    {
+      $match: { user: userId },
+    },
+
+    {
+      $facet: {
+        totalCount: [{ $count: "totalTweets" }],
+
+        tweets: [
+          { $sort: { createdAt: sortType === "desc" ? -1 : 1 } },
+          { $skip: skip },
+          { $limit: limitNum },
+
+          // Tweet lookup
+          {
+            $lookup: {
+              from: "tweets",
+              localField: "tweet",
+              foreignField: "_id",
+              as: "tweet",
+            },
+          },
+          { $unwind: "$tweet" },
+
+          // Owner lookup
+          {
+            $lookup: {
+              from: "users",
+              localField: "tweet.owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    fullName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          { $unwind: "$owner" },
+
+          // Subscriptions
+          {
+            $lookup: {
+              from: "subscriptions",
+              localField: "owner._id",
+              foreignField: "channel",
+              as: "subscribers",
+            },
+          },
+
+          // Likes
+          {
+            $lookup: {
+              from: "likes",
+              localField: "tweet._id",
+              foreignField: "tweet",
+              as: "likes",
+            },
+          },
+
+          // yaha pe hum like structure defined kr rhe hai means fields add kr rhe hai.
+          {
+            $addFields: {
+              "tweet.owner": "$owner",
+              "tweet.totalLikes": { $size: "$likes" },
+              "tweet.isLiked": {
+                $in: [userId, "$likes.likedBy"],
+              },
+              "tweet.isSubscribed": {
+                $in: [userId, "$subscribers.subscriber"],
+              },
+              "tweet.tweetSaved": true,
+            },
+          },
+
+          // tweet ko root kr rhe hai.
+          {
+            $replaceRoot: {
+              newRoot: "$tweet",
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  const tweets = result[0]?.tweets || [];
+  const totalTweets = result[0]?.totalCount[0]?.totalTweets || 0;
+
+  return res.status(200).json({
+    status: 200,
+    data: {
+      savedTweet: tweets,
+      totalTweets,
+      page: pageNum,
+      limit: limitNum,
+      totalPages: Math.ceil(totalTweets / limitNum),
+    },
+    message: "Saved Tweets fetched successfully.",
+  });
+});
+
 export { saveTweet, unsaveTweet, getSavedTweet };
